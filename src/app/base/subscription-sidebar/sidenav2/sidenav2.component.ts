d7f9356a9bb8659272a20b17291d2856f85a403e
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-04-09 18:49:09 -0700

    create sidebar for subscription

---
+++ src/app/base/subscription-sidebar/sidenav2/sidenav2.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sidenav2',
+  templateUrl: './sidenav2.component.html',
+  styleUrls: ['./sidenav2.component.css']
+})
+export class Sidenav2Component {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
