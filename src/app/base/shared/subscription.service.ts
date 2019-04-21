b728b132548aee1241c78bdec6e415a5e7ccaf87
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-04-21 13:33:49 -0700

    save before trying to implement subject

--- src/app/base/shared/subscription.service.ts
+++ src/app/base/shared/subscription.service.ts
@@ -10,7 +10,6 @@ export class SubscriptionService {
   constructor(private http: HttpClient) { }
 
   addNewSubscription(subscription: SubscriptionInterface) {
-    console.log(subscription, ' hi')
     this.http.post('http://localhost:3010/subscription/add', subscription).subscribe()
   }
 
